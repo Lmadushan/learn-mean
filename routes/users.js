07c5c06c8b77dd641f2c5bc217161ec8ec9e9129
Lahiru mirihagoda <lahiru.mirihagoda@gmail.com>
2019-04-26 08:38:03 +0530

    Bug fix

--- routes/users.js
+++ routes/users.js
@@ -55,8 +55,8 @@ router.post('/authenticate', (req, res, next) => {
     });
   });
 
-router.get('/profile', (req, res, next) => {
-    res.send('Profile');
+router.get('/profile', passport.authenticate('jwt', {session:false}),(req, res, next) => {
+    res.json({user:req.user})
 });
 
 module.exports = router;
